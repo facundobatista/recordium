19e5afcd1810b7526c4d6a92cd3110e3ce9ce77a
marian-vignau <mavignau@gmail.com>
2017-01-06 00:34:29 -0300

    close issue #3 explain and ask for config on first use

--- recordium/main.py
+++ recordium/main.py
@@ -58,12 +58,22 @@ def debug_trace():
     set_trace()
 
 
-class ConfigWidget(QtWidgets.QWidget):
+class ConfigWidget(QtWidgets.QDialog):
     """The config window."""
 
-    def __init__(self):
+    def __init__(self, explain):
         super().__init__()
         self.setWindowTitle("Configuration")
+
+        main_layout = QtWidgets.QVBoxLayout()
+        if explain:
+            main_layout.addWidget(QtWidgets.QLabel(
+                "Please configure Recordium to be able to start fetching messages\n"
+                "See instructions on README.rst"), 0)
+            hline = QtWidgets.QFrame()
+            hline.setFrameShape(QtWidgets.QFrame.HLine)
+            hline.setFrameShadow(QtWidgets.QFrame.Sunken)
+            main_layout.addWidget(hline)
 
         grid = QtWidgets.QGridLayout()
         grid.addWidget(QtWidgets.QLabel("Telegram bot auth token:"), 0, 0)
@@ -78,7 +88,9 @@ class ConfigWidget(QtWidgets.QWidget):
         self.entry_polling_time.setMinimum(1)
         grid.addWidget(self.entry_polling_time, 1, 1)
 
-        self.setLayout(grid)
+        main_layout.addLayout(grid, 0)
+
+        self.setLayout(main_layout)
         self.show()
 
     def closeEvent(self, event):
@@ -179,7 +191,8 @@ class SysTray:
 
     def _configure(self, _):
         """Show the configuration dialog."""
-        self._temp_cw = ConfigWidget()
+        self._temp_cw = ConfigWidget(explain=False)
+        self._temp_cw.exec_()
 
     def _about(self, _):
         """Show the About dialog."""
@@ -209,12 +222,17 @@ class SysTray:
 class RecordiumApp(QtWidgets.QApplication):
     def __init__(self, version):
         super().__init__(sys.argv)
-        self.setQuitOnLastWindowClosed(False)  # so app is not closed when closing other windows
-        self.storage = storage.Storage()
-        self.systray = SysTray(self, version)
-        self.messages_getter = network.MessagesGetter(
-            self._new_messages, self.storage.get_last_element_id)
-        self.messages_getter.go()
+        if not config.get(config.BOT_AUTH_TOKEN):
+            self._temp_cw = ConfigWidget(explain=True)
+            self._temp_cw.exec_()
+        if config.get(config.BOT_AUTH_TOKEN):
+            """Start application network and systray menu & icon"""
+            self.setQuitOnLastWindowClosed(False)  # so app is not closed when closing other windows
+            self.storage = storage.Storage()
+            self.systray = SysTray(self, version)
+            self.messages_getter = network.MessagesGetter(
+                self._new_messages, self.storage.get_last_element_id)
+            self.messages_getter.go()
 
     def _new_messages(self, messages):
         """Called when new messages are available."""
